6308fb03b174a0dffb20eab13ed9e2ad95cf6720
Matthew Northcott <matthewnorthcott@catalyst.net.nz>
2020-09-10 10:56:39 +1200

    [Issue #6414] Use Django storage API when generating document thumbnails

--- geonode/documents/tasks.py
+++ geonode/documents/tasks.py
@@ -31,6 +31,9 @@ from geonode.documents.renderers import generate_thumbnail_content
 from geonode.documents.renderers import ConversionError
 from geonode.documents.renderers import MissingPILError
 
+from django.conf import settings
+from django.core.files.storage import default_storage as storage
+
 logger = get_task_logger(__name__)
 
 
@@ -47,44 +50,73 @@ def create_document_thumbnail(self, object_id):
         logger.error("Document #{} does not exist.".format(object_id))
         return
 
-    image_path = None
-
-    if document.is_image():
-        image_path = document.doc_file.path
-    elif document.is_file():
-        try:
-            image_file = render_document(document.doc_file.path)
-            image_path = image_file.name
-        except ConversionError as e:
-            logger.debug("Could not convert document #{}: {}."
-                         .format(object_id, e))
+    document_path = os.path.join(settings.MEDIA_ROOT, document.doc_file.name)
+    to_dispose = []
 
     try:
-        if image_path:
-            assert isfile(image_path) and access(image_path, R_OK) and os.stat(image_path).st_size > 0
-    except (AssertionError, TypeError):
+        if not os.path.exists(document_path):
+            if storage.exists(document.doc_file.name):
+                # Save document to expected local path temporarily
+                parent = os.path.dirname(document_path)
+                if not os.path.exists(parent):
+                    os.makedirs(parent)
+
+                f_src = storage.open(document.doc_file.name, 'rb')
+                f_dst = open(document_path, 'wb+')
+                buf = f_src.read(4096)
+
+                while buf:
+                    f_dst.write(buf)
+                    buf = f_src.read(4096)
+
+                f_dst.close()
+                f_src.close()
+                to_dispose.append(document_path)  # Dispose of temp doc when done
+            else:
+                logger.error("Document #{} exists but its path could not be resolved.".format(object_id))
+                return
+
         image_path = None
 
-    if not image_path:
-        image_path = document.find_placeholder()
+        if document.is_image():
+            image_path = document_path
+        elif document.is_file():
+            try:
+                image_path = render_document(document_path)
+                to_dispose.append(image_path)
+            except ConversionError as e:
+                logger.debug("Could not convert document #{}: {}."
+                            .format(object_id, e))
 
-    if not image_path or not os.path.exists(image_path):
-        logger.debug("Could not find placeholder for document #{}"
-                     .format(object_id))
-        return
+        try:
+            if image_path:
+                assert isfile(image_path) and access(image_path, R_OK) and os.stat(image_path).st_size > 0
+        except (AssertionError, TypeError):
+            image_path = None
 
-    thumbnail_content = None
-    try:
-        thumbnail_content = generate_thumbnail_content(image_path)
-    except MissingPILError:
-        logger.error('Pillow not installed, could not generate thumbnail.')
-        return
+        if not image_path:
+            image_path = document.find_placeholder()
 
-    if not thumbnail_content:
-        logger.warning("Thumbnail for document #{} empty.".format(object_id))
-    filename = 'document-{}-thumb.png'.format(document.uuid)
-    document.save_thumbnail(filename, thumbnail_content)
-    logger.debug("Thumbnail for document #{} created.".format(object_id))
+        if not image_path or not os.path.exists(image_path):
+            logger.debug("Could not find placeholder for document #{}"
+                        .format(object_id))
+            return
+
+        thumbnail_content = None
+        try:
+            thumbnail_content = generate_thumbnail_content(image_path)
+        except MissingPILError:
+            logger.error('Pillow not installed, could not generate thumbnail.')
+            return
+
+        if not thumbnail_content:
+            logger.warning("Thumbnail for document #{} empty.".format(object_id))
+        filename = 'document-{}-thumb.png'.format(document.uuid)
+        document.save_thumbnail(filename, thumbnail_content)
+        logger.debug("Thumbnail for document #{} created.".format(object_id))
+    finally:
+        for _path in to_dispose:
+            os.remove(_path)
 
 
 @app.task(bind=True, queue='cleanup')
